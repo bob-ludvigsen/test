7d476b979e0e635cc0edf8c05cefa8a65659e6af
bob-ludvigsen <bob@hesse-ludvigsen.dk>
2015-06-12 10:15:21 +0200

    Initial

---
+++ testQuiz.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bob on 11/06/15.
+ */
+
+$(document).ready(function () {
+    var a1 = [1, 3, 5];
+    var a2 = [2, 4, 6];
+    $.merge(a1, a2);
+    alert(typeof $.trim());
+
+
+})
